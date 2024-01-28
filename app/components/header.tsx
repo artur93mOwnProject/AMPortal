cf3b188e32af673d159845cae402ef511d18dca4
Artur M <artur93mm@gmail.com>
2024-01-28 10:14:50 +0100

    Added header menu

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,16 +4,19 @@ import classes from "./header.module.css";
 
 export default function Header() {
   return (
-    <ul className={classes.menu}>
-      <li>
-        <Link href={"/about"}>AboutMe</Link>
-      </li>
-      <li>
-        <Link href={"/portfolio"}>Portfolio</Link>
-      </li>
-      <li>
-        <Link href={"/contact"}>Contact</Link>
-      </li>
-    </ul>
+    <div className={classes.header}>
+      <span>AMPortal</span>
+      <ul className={classes.menu}>
+        <li>
+          <Link href={"/about"}>AboutMe</Link>
+        </li>
+        <li>
+          <Link href={"/portfolio"}>Portfolio</Link>
+        </li>
+        <li>
+          <Link href={"/contact"}>Contact</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
